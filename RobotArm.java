f275a705c96759d67ba5e187854d851c42013f80
Bailey Jewell <jewellbail@myvuw.ac.nz>
2018-10-09 14:30:35 +1300

    Started code outline

---
+++ RobotArm.java
@@ -0,0 +1,29 @@
+class RobotArm {
+    
+    public RobotArm() {}
+    
+    private ArrayList<ArrayList<Boolean>> image;
+
+    //// DRAWING METHODS ////
+
+    /**
+     * Draws a straight line from (x1, y1) to (x2, y2) 
+     */
+    private void drawLine(double x1, double y1, double x2, double y2) {}
+
+    /**
+     * Draws a circle centred at centre, with radius radius
+     */
+    private void drawCircle(double centre, double radius) {}
+
+
+
+    
+    
+
+    
+
+    public static void main(String[] args) {
+        System.out.println("RobotArm started!");
+    }
+}
