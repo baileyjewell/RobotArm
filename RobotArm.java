aeff9290ff8a1514b398a9a8f077f555f421b43b
dolfinn1212 <finn.coleman@hotmail.com>
2018-10-11 15:05:28 +1300

    Draw image method and moveTo+drawLine structure

--- RobotArm.java
+++ RobotArm.java
@@ -3,13 +3,25 @@ class RobotArm {
     public RobotArm() {}
     
     private ArrayList<ArrayList<Boolean>> image;
+    
+    // holds (x, y) in order of drawing
+    private ArrayList<ArrayList<Double>> drawOrder;
 
     //// DRAWING METHODS ////
 
     /**
      * Draws a straight line from (x1, y1) to (x2, y2) 
      */
-    private void drawLine(double x1, double y1, double x2, double y2) {}
+    private void drawLine(double x1, double y1, double x2, double y2) {
+        if (currentX != x1 || currentY != y1) {
+            moveTo(x1, y1);
+        }
+
+        // draw line between (x1, y1) and (x2, y2)
+
+        currentX = x2;
+        currentY = y2;
+    }
 
     /**
      * Draws a circle centred at centre, with radius radius
@@ -19,10 +31,25 @@ class RobotArm {
     /**
      * Lifts the pen and moves in a straight line from (x1, y1) to (x2, y2)
      */
-    private void moveTo(double x1, double y1, double x2, double y2) {}
+    private void moveTo(double x1, double y1, double x2, double y2) {
+        // pen up
+        // move from (currentX, currentY) to (x2, y2)
+        // pen down
+
+        currentX = x2;
+        currentY = y2;
+ 
+     }
 
     private void drawImage() {
-
+        double x2 = drawOrder.get(0).get(0); // next point to draw
+        double y2 = drawOrder.get(0).get(1);
+        moveTo(x2, y2);
+        for (int i = 1; i < drawOrder.size(); i++) {
+            x2 = drawOrder.get(i).get(0);
+            y2 = drawOrder.get(i).get(1);
+            drawLine(currentX, currentY, x2, y2);
+        }
     }
 
     
