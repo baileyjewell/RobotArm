891e817d65d499b908c577d72d4f3f6017240d45
dolfinn1212 <finn.coleman@hotmail.com>
2018-10-11 14:22:40 +1300

    Image drawing method and lifting+moving method

--- RobotArm.java
+++ RobotArm.java
@@ -16,7 +16,14 @@ class RobotArm {
      */
     private void drawCircle(double centre, double radius) {}
 
+    /**
+     * Lifts the pen and moves in a straight line from (x1, y1) to (x2, y2)
+     */
+    private void moveTo(double x1, double y1, double x2, double y2) {}
 
+    private void drawImage() {
+
+    }
 
     
 
